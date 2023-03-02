26014a6903661f6247606329aed4c0cc17a3b324
Nakkarin Laokhonkha <6331305012@lamduan.mfu.ac.th>
2023-03-02 12:40:23 +0700

    test1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="ยินดีต้อรับสู่ความไม่มีอะไรเลย"/>
   </div>
 </template>
 
